e0fec7a2d5cdb4b4d45e2f3a9ce516817bcf66b5
sakil-sagor <sakilsagor76543@gmail.com>
2023-09-26 09:41:55 +0600

    fix navbar reload issue

--- src/components/Navbar/LinkBar.jsx
+++ src/components/Navbar/LinkBar.jsx
@@ -4,7 +4,9 @@ import { NavLink } from 'react-router-dom';
 const LinkBar = ({ route, open, setOpen }) => {
     return (
         <li className='ml-2 mt-10 md:mt-0 active border md:border-0  nav-bg '>
-            <NavLink className="block py-2" onClick={() => setOpen(!open)} to={route?.path}>{route?.name}</NavLink>
+            <NavLink to={route?.path}>
+                <span className="block py-2" onClick={() => setOpen(!open)} >{route?.name}</span>
+            </NavLink>
         </li>
     );
 };
